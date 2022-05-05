416048fadb5b11b501780e979ee32c20ce80e6e9
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-05-05 14:56:10 +0200

    Sun spinning on it's Y axis

--- src/components/animatedSphere.js
+++ src/components/animatedSphere.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { Sphere } from '@react-three/drei';
 import Texture from '../assets/images/sun.webp';
-import { useLoader } from '@react-three/fiber';
+import { useLoader, useFrame } from '@react-three/fiber';
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
 
-export default function AnimatedSphere() {
+export default function AnimatedSphere(props) {
+  const sphere = useRef();
+  useFrame((state, delta) => (sphere.current.rotation.y += 0.005));
+
   const colorMap = useLoader(TextureLoader, Texture);
   return (
-    <Sphere visible args={[1, 200, 400]} scale={2}>
+    <Sphere ref={sphere} visible args={[1, 200, 400]} scale={2}>
       <meshStandardMaterial map={colorMap} />
       <pointLight color="orange" intensity={4} distance={500}/>
       <ambientLight intensity={0.3} />
