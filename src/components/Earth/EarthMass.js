2563243a2d426b04f70382f5f22e641c3f16cd07
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-05-10 19:40:21 +0200

    Attached text on planet

--- src/components/Earth/EarthMass.js
+++ src/components/Earth/EarthMass.js
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import Texture from '../../assets/images/earth.jpeg';
 import { useLoader, useFrame } from '@react-three/fiber';
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
+import { Html } from '@react-three/drei';
 
 export default function EarthMass(props) {
   const colorMap = useLoader(TextureLoader, Texture);
@@ -12,6 +13,12 @@ export default function EarthMass(props) {
     <mesh ref={mesh} position={[0, 0, 13]} scale={1}>
       <sphereBufferGeometry attach="geometry" args={[1, 100, 100]} />
       <meshStandardMaterial map={colorMap} />
+      <Html>
+        <div>
+          <h1 style={{ color: 'white' }}>hel</h1>
+          <p style={{ color: 'white' }}>text</p>
+        </div>
+      </Html>
     </mesh>
   );
 }
