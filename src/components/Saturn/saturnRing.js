90cce115f25476250c3e80ea8e0904280408b525
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-05-10 08:59:52 +0200

    Ring on Saturn

--- src/components/Saturn/saturnRing.js
+++ src/components/Saturn/saturnRing.js
@@ -1,19 +1,17 @@
 import React, { useRef } from 'react';
-import Texture from '../../assets/images/saturn.webp';
+import Texture from '../../assets/images/saturn_ring.webp';
 import { useLoader, useFrame } from '@react-three/fiber';
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
-import Ring from "./saturnRing"
 
 export default function SaturnMass(props) {
   const colorMap = useLoader(TextureLoader, Texture);
   const mesh = useRef();
-  useFrame((state, delta) => (mesh.current.rotation.y += 0.01));
+  //useFrame((state, delta) => (mesh.current.rotation.y += 0.01));
 
   return (
-    <mesh ref={mesh} position={[0, 0, 7]} scale={0.7}>
-      <sphereBufferGeometry attach="geometry" args={[1, 100, 100]} />
+    <mesh ref={mesh} position={[0, 0, 0]} scale={0.7}>
+      <ringGeometry attach="geometry" args={[2, 3, 32]} />
       <meshStandardMaterial map={colorMap} />
-      <Ring/>
     </mesh>
   );
 }
