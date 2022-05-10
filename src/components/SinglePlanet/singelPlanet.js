3f23463b8997fe367bf83286f7f744b6c88f9148
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-05-10 13:23:53 +0200

    New structure including routes

---
+++ src/components/SinglePlanet/singelPlanet.js
@@ -0,0 +1,21 @@
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+ import TextCard from '../textCard';
+
+function SinglePlanet() {
+  return (
+    <div className="App">
+      <Canvas>
+        <OrbitControls enableZoom={true} />
+        <Stars />
+        <ambientLight intensity={0.5} />
+        <Suspense>
+          <TextCard/>
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
+
+export default SinglePlanet;
