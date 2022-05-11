56b4334192c9289aeee1ff13e92a6a6c247e5954
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-05-11 12:13:46 +0200

    More info on SinglePlanet

--- src/components/SinglePlanet/singelPlanet.js
+++ src/components/SinglePlanet/singelPlanet.js
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 
 function SinglePlanet(props) {
   const [planet, setPlanet] = useState([]);
+
   let params = useParams();
 
   useEffect(() => {
@@ -21,7 +22,13 @@ function SinglePlanet(props) {
         <Stars />
         <ambientLight intensity={0.7} />
         <Suspense>
-          <TextCard name={planet.englishName} text={planet.bodyType} />
+          <TextCard
+          name={planet.englishName}
+          text={planet.bodyType}
+          gravity={planet.gravity}
+          orbit={planet.sideralOrbit}
+          hours={planet.sideralRotation}/>
+
         </Suspense>
       </Canvas>
     </div>
