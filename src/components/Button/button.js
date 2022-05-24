a85fc70f9de2bdc7e5d4b354de313d71ccfb5aa9
Sofia Dersén <Sofia_dersen@hotmail.com>
2022-05-24 20:38:41 +0200

    New orbit-btn and fix on introtext

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,6 +1,6 @@
 export default function Button({ handleClick }) {
   return (
-    <mesh position={[1.2, 10, 0]} onClick={handleClick}>
+    <mesh position={[1.2, 100, 0]} onClick={handleClick}>
       <boxGeometry args={[10, 1, 1]} />
       <meshStandardMaterial color='hotpink' />
     </mesh>
---
+++ src/components/Button/buttonTwo.js
@@ -0,0 +1,16 @@
+const btnStyle = {
+  width: '100px',
+  backgroundColor: 'transparent',
+  color: 'white',
+  border: 'solid 2px white',
+  borderRadius: '10px',
+  marginLeft: "20px"
+};
+
+export default function Button({ handleClick, text }) {
+  return (
+    <button style={btnStyle} onClick={handleClick}>
+      {text}
+    </button>
+  );
+}
