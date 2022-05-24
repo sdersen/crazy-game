5bf241930ec92bfbbae5f4d84582f27ba4907e16
sowulff <sowu99@gmail.com>
2022-05-24 11:02:33 +0200

    Update venusMass.js

--- src/components/Venus/venusMass.js
+++ src/components/Venus/venusMass.js
@@ -6,10 +6,10 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader';
 export default function VenusMass(props) {
   const colorMap = useLoader(TextureLoader, Texture);
   const mesh = useRef();
-  useFrame((state, delta) => (mesh.current.rotation.y += 0.01));
+  useFrame((state, delta) => (mesh.current.rotation.y += 0.02));
 
   return (
-    <mesh ref={mesh} position={[0, 0, 20]} scale={1}>
+    <mesh ref={mesh} position={[10, 0, 20]} scale={1}>
       <sphereBufferGeometry attach="geometry" args={[1, 100, 100]} />
       <meshStandardMaterial map={colorMap} />
     </mesh>
